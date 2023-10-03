ff99221e0611e61b4880d16c247db8d8bb071ace
IYATT-yx <iyatt@iyatt.com>
2023-10-03 13:00:37 +0800

    refactor: 将日志文件路径定义迁移到该文件; 定义风扇调速周期

--- parameter.h
+++ parameter.h
@@ -5,11 +5,13 @@
 #ifndef PARAMETER_H
 #define PARAMETER_H
 
-#define PWM_PIN     18 // PWM 引脚
-#define PWM_FREQ    20 // PWM 频率
-#define PWM_RANGE   1023 // PWM 范围 0-1023
-#define VOLT_MIN    0.0f // PWM 最低电压
-#define VOLT_MAX    3.3f // PWM 最高电压
-#define CLOCK_FREQ  19200000 // PWM 控制器时钟频率
+#define PWM_PIN                 18                  // PWM 引脚
+#define PWM_FREQ                20                  // PWM 频率
+#define PWM_RANGE               1023                // PWM 范围 0-1023
+#define VOLT_MIN                0.0f                // PWM 最低电压
+#define VOLT_MAX                3.3f                // PWM 最高电压
+#define CLOCK_FREQ              19200000            // PWM 控制器时钟频率
+#define ERROR_LOG_FILE_PATH     "/tmp/pi-fan.log"   // 日志文件保存路径
+#define DELAY                   5000                // 风扇调速周期，单位毫秒
 
 #endif
