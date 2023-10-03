403b7ebea6e447636c31978ea710f264cf69e778
IYATT-yx <iyatt@iyatt.com>
2023-10-03 12:40:18 +0800

    chore: 修改为依赖环境缺失才安装

--- install.sh
+++ install.sh
@@ -38,11 +38,46 @@ function install()
 	systemctl status pi-fan.service
 }
 
+# 检查命令工具是否安装
+function check_command()
+{
+	command -v "$1" >/dev/null 2>&1
+}
+
+# 检查进程是否在运行
+function check_process()
+{
+	pgrep "$1" >/dev/null 2>&1
+}
+
 function main
 {
-	install_build_tool
-	install_wiringpi
+	# 检查是否安装构建工具
+	commands=("git" "make" "gcc")
+	for cmd in "${commands[@]}"
+	do
+		if ! check_command "$cmd"
+		then
+			install_build_tool
+		fi
+	done
+
+	# 检查是否安装 WiringPi
+	if ! check_command "gpio"
+	then
+		install_wiringpi
+	fi
+
+	# 构建
 	build
+
+	# 有正在运行的实例就关闭
+	if check_process "pi-fan"
+	then
+		sudo systemctl stop pi-fan.service
+	fi
+
+	# 安装
 	install
 }
 
